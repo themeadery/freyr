0ecc9eedce33ff80fe3b0e09500336d7b194dd40
Matt Mead <matt.a.mead@gmail.com>
2021-09-21 11:26:29 -0700

    cleanup

--- tnh_lite_loop-public.py
+++ tnh_lite_loop-public.py
@@ -2,11 +2,9 @@ import time
 import board
 import adafruit_si7021
 import requests
-import json
 
 query = {'lat':'put your latitude here', 'lon':'put your longitude here', 'appid':'put your API key here'}
 sensor = adafruit_si7021.SI7021(board.I2C())
-#hasl = 217 # Height above sea-level for weather station in meters
 
 # Main Loop
 while True:
@@ -24,12 +22,6 @@ while True:
         print("Temperature: %0.2f °F" % tempf)
         hum = main['humidity']
         print("Humidity: %0.1f%%" % hum)
-        # press = main['pressure']
-        # print("Aggregate Uncorrected Station Pressures: %0.2f hPA" % press)
-        # slpress = press + ((press * 9.80665 * hasl)/(287 * (273 + tempc + (hasl/400))))
-        # print("Sea-Level Pressure: %0.2f hPA" % slpress)
-        # slpress = (press * pow(1 - (0.0065 * hasl / (tempc + 0.0065 * hasl + 273.15)), -5.257))
-        # print("Sea-Level Pressure: %0.2f hPA" % slpress)
         # Code above here will only run if the request is successful
     except requests.exceptions.HTTPError as errh:
         print(errh)
