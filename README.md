25be7222be501a01716d3ba190bdf3324eec0b7d
Matt Mead <matt.a.mead@gmail.com>
2021-11-05 13:56:03 -0700

    RRD Graphs
    Major update including RRD graph functionality

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 This started out as a project to control my reef aquarium, hence the name. It then morphed into a simple Python script to check the indoor temp/humidity and compare it with the outdoors using OpenWeather API.
 
-The hardware is a FT232H from Adafruit and a Si7021 sensor. The FT232H is a pain in the butt to set up from scratch, espcially on Windows. I use Zadig to change the driver to libusbK, then the environment variable must be set "BLINKA_FT232H=1". Use the hack to refresh the environment variables without rebooting (now included, see RefreshEnv.cmd), then the Python script should work. If you ever switch USB ports you must use Zadig to change the driver again.
+The hardware is a FT232H from Adafruit and a Si7021 sensor. The FT232H is a pain in the butt to set up from scratch, espcially on Windows. I use Zadig to change the driver to libusbK, then the environment variable must be set "BLINKA_FT232H=1". Use the hack to refresh the environment variables without rebooting (now included, see ```RefreshEnv.cmd```), then the Python script should work. If you ever switch USB ports you must use Zadig to change the driver again.
 
 Adafruit has written a guide for the FT232H, but it is confusing and becoming out of date: https://learn.adafruit.com/circuitpython-on-any-computer-with-ft232h
 
-Requires:
+## Requires:
 - Python 3
 - pip
 - pyftdi
@@ -22,5 +22,21 @@ Requires:
 - certifi
 - urllib3
 - idna
+- rrdtool (added to system $PATH/%PATH%/Path)
 
-At one point it created RRD graphs and updated a local website, but that feature is currently broken.
+## RRD
+#### Create RRD databases:
+
+```
+$ rrdtool create temperatures-c.rrd --step 60 DS:outdoor:GAUGE:120:0:55 DS:indoor:GAUGE:120:0:55 RRA:MAX:0.5:1:1440
+$ rrdtool create humidities.rrd --step 60 DS:outdoor:GAUGE:120:0:100 DS:indoor:GAUGE:120:0:100 RRA:MAX:0.5:1:1440
+```
+This will create databases with a 60 second interval, 120 second hearbeat timeout, between 0 and 55 degrees Celsius, and 0-100% relative humidity, with 24 hours of data before rolling over. You may need to configure for lower than 0 degrees Celsius, but I live in the second hottest place on the planet so these are my settings.
+
+More information here: https://michael.bouvy.net/post/graph-data-rrdtool-sensors-arduino
+
+## HTML
+
+```index.html```
+
+Very simple HTML to display both graphs. This does not need a webserver. It can be opened locally and will refresh the entire page every 60 seconds.
---
+++ reefer.py
@@ -0,0 +1,59 @@
+import time
+import board
+import adafruit_si7021
+import requests
+import subprocess
+
+query = {'lat':'put your latitude here', 'lon':'put your longitude here', 'appid':'put your API key here'}
+sensor = adafruit_si7021.SI7021(board.I2C())
+
+# Main Loop
+while True:
+    print("\n--------------------------------")
+    print("\nOutdoor")
+    try:
+        response = requests.get('https://api.openweathermap.org/data/2.5/weather', params=query, timeout=5)
+        response.raise_for_status()
+        # Code below here will only run if the request is successful
+        main = response.json()['main']
+        tempk = main['temp']
+        tempc = tempk-273.14
+        print("Temperature: %0.2f °C" % tempc)
+        tempf = tempc*9/5+32
+        print("Temperature: %0.2f °F" % tempf)
+        hum = main['humidity']
+        print("Humidity: %0.1f%%" % hum)
+        # Code above here will only run if the request is successful
+    except requests.exceptions.HTTPError as errh:
+        print(errh)
+    except requests.exceptions.ConnectionError as errc:
+        print(errc)
+    except requests.exceptions.Timeout as errt:
+        print(errt)
+    except requests.exceptions.RequestException as err:
+        print(err)
+
+    print("\nIndoor")
+    fahrenheit = sensor.temperature*9/5+32
+    print("Temperature: %0.2f °C" % sensor.temperature)
+    print("Temperature: %0.2f °F" % fahrenheit)
+    print("Humidity: %0.1f%%" % sensor.relative_humidity)
+
+    print("\nUpdating RRD databases...")
+    subprocess.run(["rrdtool", "update", "temperatures-c.rrd", "N:%0.2f" % tempc + ":%0.2f" % sensor.temperature])
+    subprocess.run(["rrdtool", "update", "humidities.rrd", "N:%0.1f" % hum + ":%0.1f" % sensor.relative_humidity])
+    print("Done")
+
+    print("\nCreating graphs...")
+    subprocess.run(["rrdtool", "graph","temperatures.png", "--font", "DEFAULT:10:", "--title", "Temperature",
+     "--vertical-label", "Celsius", "--right-axis-label", "Fahrenheit", "--right-axis", "1.8:32",
+     "DEF:outdoor=temperatures-c.rrd:outdoor:MAX", "DEF:indoor=temperatures-c.rrd:indoor:MAX",
+     "LINE1:outdoor#ff0000:Outdoor", "LINE1:indoor#0000ff:Indoor", "--width", "600", "--height", "220",
+     "--alt-autoscale"])
+    subprocess.run(["rrdtool", "graph", "humidities.png", "--font", "DEFAULT:10:", "--title", "Humidity",
+     "--vertical-label", "Relative (%)", "DEF:outdoor=humidities.rrd:outdoor:MAX", "DEF:indoor=humidities.rrd:indoor:MAX",
+     "LINE1:outdoor#ff0000:Outdoor", "LINE1:indoor#0000ff:Indoor", "--width", "600", "--height", "200",
+     "--alt-autoscale"])
+    print("Done")
+
+    time.sleep(60)
