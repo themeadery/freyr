4695be3011ddf45f0c404dc463d6c265bfcee4b4
Matt Mead <matt.a.mead@gmail.com>
2024-01-01 16:18:32 -0700

    Update boot.py
    Lights up onboard LED when wlan is connected.

--- Pi Pico W/boot.py
+++ Pi Pico W/boot.py
@@ -2,11 +2,19 @@ import webrepl
 import network
 import secrets
 from utime import sleep
+from machine import Pin
+
+led = machine.Pin("LED", machine.Pin.OUT)
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True) # power up the WiFi chip
 sleep(3) # wait X seconds for the chip to power up and initialize
 wlan.connect(secrets.SSID, secrets.PASSWORD)
-sleep(6)
+sleep(6) # wait for connection handshake to finish
+if wlan.isconnected():
+    print("wlan connected")
+    led.on()
+else:
+    print("wlan connection error")
 
 webrepl.start()
