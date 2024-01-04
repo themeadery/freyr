19a0f0dd8c1f9a2cac9f84b7b6a151b88f719741
Matt Mead <matt.a.mead@gmail.com>
2024-01-03 21:10:36 -0700

    Microdot debugging
    Add debugging code to try to catch error when running a long time and print more info to the webrepl.

--- Pi Pico W/main.py
+++ Pi Pico W/main.py
@@ -1,3 +1,4 @@
+import time
 from machine import Pin, I2C
 from micropython_si7021 import si7021
 from microdot import Microdot
@@ -8,9 +9,18 @@ si = si7021.SI7021(i2c)
 
 app = Microdot()
 
+@app.before_request
+async def start_timer(request):
+    request.g.start_time = time.ticks_ms()
+
+@app.after_request
+async def end_timer(request, response):
+    duration = time.ticks_diff(time.ticks_ms(), request.g.start_time)
+    print(f'Request took {duration} ms')
+
 @app.get('/')
 async def index(request):
     return {'temperature': si.temperature,
             'humidity': si.humidity}
 
-app.run(port=80)
+app.run(port=80, debug=True)
