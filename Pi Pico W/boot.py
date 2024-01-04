60907cfc047e00b14c0c430bb23509beb175f481
Matt Mead <matt.a.mead@gmail.com>
2024-01-04 16:43:24 -0700

    faster boot.py script
    I don't even know how the old version worked because the LED section was wrong. Should loop until connected and connect faster than horrible old version.

--- Pi Pico W/boot.py
+++ Pi Pico W/boot.py
@@ -2,19 +2,20 @@ import webrepl
 import network
 import secrets
 from utime import sleep
-from machine import Pin
+import machine
 
 led = machine.Pin("LED", machine.Pin.OUT)
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True) # power up the WiFi chip
 sleep(3) # wait X seconds for the chip to power up and initialize
-wlan.connect(secrets.SSID, secrets.PASSWORD)
-sleep(6) # wait for connection handshake to finish
-if wlan.isconnected():
-    print("wlan connected")
-    led.on()
-else:
-    print("wlan connection error")
+
+while not wlan.isconnected():
+    print("Trying to connect to WLAN...")
+    wlan.connect(secrets.SSID, secrets.PASSWORD)
+    sleep(2)
+
+print("WLAN connected")
+led.on()
 
 webrepl.start()
