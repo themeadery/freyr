b86021794e26d4f0bb6287ef0f381f5b6bfccf30
Matt Mead <matt.a.mead@gmail.com>
2024-01-04 21:10:12 -0700

    cool blink code if it can't connect

--- Pi Pico W/boot.py
+++ Pi Pico W/boot.py
@@ -8,12 +8,15 @@ led = machine.Pin("LED", machine.Pin.OUT)
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True) # power up the WiFi chip
-sleep(3) # wait X seconds for the chip to power up and initialize
+sleep(2) # wait X seconds for the chip to power up and initialize
 
 while not wlan.isconnected():
     print("Trying to connect to WLAN...")
     wlan.connect(secrets.SSID, secrets.PASSWORD)
-    sleep(2)
+    led.on()
+    sleep(0.1)
+    led.off()
+    sleep(1.4)
 
 print("WLAN connected")
 led.on()
