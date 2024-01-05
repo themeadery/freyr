c051f5112bc3b63f62cdea0f4cff3c07cf09510c
Matt Mead <matt.a.mead@gmail.com>
2024-01-05 12:51:36 -0700

    Less aggressive boot.py
    Was working fine yesterday, not working today. Go figure.

--- Pi Pico W/boot.py
+++ Pi Pico W/boot.py
@@ -8,7 +8,7 @@ led = machine.Pin("LED", machine.Pin.OUT)
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True) # power up the WiFi chip
-sleep(2) # wait X seconds for the chip to power up and initialize
+sleep(3) # wait X seconds for the chip to power up and initialize
 
 while not wlan.isconnected():
     print("Trying to connect to WLAN...")
@@ -16,7 +16,7 @@ while not wlan.isconnected():
     led.on()
     sleep(0.1)
     led.off()
-    sleep(1.4)
+    sleep(5)
 
 print("WLAN connected")
 led.on()
