2f5a12221a016b8d11fc12b642c7e78ea7d23934
Matt Mead <matt.a.mead@gmail.com>
2024-01-04 17:26:22 -0700

    debug code
    Added debug code to Pico W main.py. Diagnosing a possible memory leak. Won't know what the problem is for days, but this debug code works great. There was what looked like a leak until I added the gc.collect() function. Haven't had a stable 24 hours, yet, but this will take days to diagnose.

--- Pi Pico W/main.py
+++ Pi Pico W/main.py
@@ -2,6 +2,8 @@ import time
 from machine import Pin, I2C
 from micropython_si7021 import si7021
 from microdot import Microdot
+#import micropython
+import gc
 
 # Initialize si7021 sensor
 i2c = I2C(0, sda=Pin(0), scl=Pin(1))  # i2c0 pins for Pico
@@ -12,11 +14,22 @@ app = Microdot()
 @app.before_request
 async def start_timer(request):
     request.g.start_time = time.ticks_ms()
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 @app.after_request
 async def end_timer(request, response):
     duration = time.ticks_diff(time.ticks_ms(), request.g.start_time)
     print(f'Request took {duration} ms')
+    #print('micropython.mem_info():')
+    #micropython.mem_info()
+    print(f'gc.mem_alloc(): {gc.mem_alloc()}')
+    print(f'gc.mem_free(): {gc.mem_free()}')
+    print('Taking out the garbage...')
+    gc.collect()
+    #print('micropython.mem_info() after gc.collect():')
+    #micropython.mem_info()
+    print(f'gc.mem_alloc(): {gc.mem_alloc()}')
+    print(f'gc.mem_free(): {gc.mem_free()}')
 
 @app.get('/')
 async def index(request):
