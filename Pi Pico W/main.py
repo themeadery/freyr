788152cb7552747ee6dfabd15a860bc44365bf41
Matt Mead <matt.a.mead@gmail.com>
2024-01-05 21:39:25 -0700

    New si7021 driver
    Updated si7021 driver to what I feel is a better written driver.  Performance improved by 5 ms on a full loop of main.py. I like that I can get a temp reading by re-using the temp reading during the humidity reading instead of polling the sensor again. Error handling seems better written. T+7 hours in testing without fault, will commit again if a failure occurs. Better repl logging, removed old debug code.

--- Pi Pico W/main.py
+++ Pi Pico W/main.py
@@ -1,13 +1,12 @@
 import time
 from machine import Pin, I2C
-from micropython_si7021 import si7021
+from SI7021 import SI7021
 from microdot import Microdot
-#import micropython
 import gc
 
 # Initialize si7021 sensor
 i2c = I2C(0, sda=Pin(0), scl=Pin(1))  # i2c0 pins for Pico
-si = si7021.SI7021(i2c)
+si = SI7021(i2c)
 
 app = Microdot()
 
@@ -20,20 +19,20 @@ async def start_timer(request):
 async def end_timer(request, response):
     duration = time.ticks_diff(time.ticks_ms(), request.g.start_time)
     print(f'Request took {duration} ms')
-    #print('micropython.mem_info():')
-    #micropython.mem_info()
     print(f'gc.mem_alloc(): {gc.mem_alloc()}')
     print(f'gc.mem_free(): {gc.mem_free()}')
     print('Taking out the garbage...')
     gc.collect()
-    #print('micropython.mem_info() after gc.collect():')
-    #micropython.mem_info()
     print(f'gc.mem_alloc(): {gc.mem_alloc()}')
     print(f'gc.mem_free(): {gc.mem_free()}')
 
 @app.get('/')
 async def index(request):
-    return {'temperature': si.temperature,
-            'humidity': si.humidity}
+    humidity = si.humidity()
+    temperature = si.temperature(new=False)
+    print(f'Temperature: {temperature} °C')
+    print(f'Humidity: {humidity} %')
+    return {'temperature': temperature,
+            'humidity': humidity}
 
 app.run(port=80, debug=True)
