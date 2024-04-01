c2a08af41f0310a1d442e7f9ef5e802114b6cf8d
Matt Mead <matt.a.mead@gmail.com>
2024-04-01 10:39:57 -0700

    main.py debug toggle
    Added a way to turn off all debugging messages that are printed to the WebREPL console. Might speed things up.

--- Pi Pico W/main.py
+++ Pi Pico W/main.py
@@ -4,6 +4,8 @@ from SI7021 import SI7021
 from microdot import Microdot
 import gc
 
+debug = False # set to True to turn on debugging info below
+
 # Initialize si7021 sensor
 i2c = I2C(0, sda=Pin(0), scl=Pin(1))  # i2c0 pins for Pico
 si = SI7021(i2c)
@@ -27,22 +29,25 @@ async def start_timer(request):
 @app.after_request
 async def end_timer(request, response):
     duration = time.ticks_diff(time.ticks_ms(), request.g.start_time)
-    print(f'Request took {duration} ms')
-    print(f'gc.mem_alloc(): {gc.mem_alloc()}')
-    print(f'gc.mem_free(): {gc.mem_free()}')
-    print('Taking out the garbage...')
+    if debug:
+        print(f'Request took {duration} ms')
+        print(f'gc.mem_alloc(): {gc.mem_alloc()}')
+        print(f'gc.mem_free(): {gc.mem_free()}')
+        print('Taking out the garbage...')
     gc.collect()
-    print(f'gc.mem_alloc(): {gc.mem_alloc()}')
-    print(f'gc.mem_free(): {gc.mem_free()}')
+    if debug:
+        print(f'gc.mem_alloc(): {gc.mem_alloc()}')
+        print(f'gc.mem_free(): {gc.mem_free()}')
 
 @app.get('/')
 async def index(request):
     humidity = si.humidity()
     temperature = si.temperature(new=False)
     mcu_temp = read_mcu_temp()
-    print(f'Temperature: {temperature} °C')
-    print(f'Humidity: {humidity} %')
-    print(f'MCU Temperature: {mcu_temp} °C')
+    if debug:
+        print(f'Temperature: {temperature} °C')
+        print(f'Humidity: {humidity} %')
+        print(f'MCU Temperature: {mcu_temp} °C')
     return {'temperature': temperature,
             'humidity': humidity, 'mcu': mcu_temp}
 
